68ab0db11ae472a44d0de39809006fd3501d4457
MarkLambson <mj.lambson@yahoo.com>
2023-10-10 15:27:01 -0700

    and more loops

--- loops.js
+++ loops.js
@@ -1,30 +1,50 @@
+console.log("answer to question 1")
 // 1. Print odd numbers from 1 to 20 -----------------------------//
-for(let i = 1; i <= 20; i++) {
-    // for loop iterating through 1 to 20
-    let even = [];
-    //array to store even numbers, not doing anything with them
-    if (i % 2 === 0) {
-        even.push(i);
-        //if the number is divisible by 2, push it to the array
-    } else {
-        console.log(i);
-        //otherwise, console log it!
+for (let i = 1; i <= 20; i++) {
+    if (i % 2 !== 0) {
+        console.log(i)
     }
 }
 //---------------------------------------------------------------//
 
+console.log("-------------------------------------")
 
-
-// 2. Decreasing multiples of 3, starting at 100 and ending at 0--------------------------------//
-for(let i = 100; i > 0; i-=3) {
+console.log("answer to question 2")
+// 2. Decreasing multiples of 3, starting at 100 and ending at 0 //
+for (let i = 100; i > 0; i -= 3) {
     console.log(i)
 }
 //---------------------------------------------------------------//
 
+console.log("-------------------------------------")
 
-
+console.log("answer to question 3")
 // 3. Print this sequence: 4, 2.5, 1, -0.5, -2, -3.5
-for(let i = 4; i > -4; i-=1.5) {
+for (let i = 4; i > -4; i -= 1.5) {
     console.log(i)
 }
 //---------------------------------------------------------------//
+
+console.log("-------------------------------------")
+
+console.log("answer to question 4")
+// 4. Add all values from 1 to 100 together and display answer //
+var sum = 0;
+for (var i = 1; i < 101; i++) {
+    sum += i;
+}
+console.log(sum);
+//---------------------------------------------------------------//
+
+console.log("-------------------------------------")
+
+console.log("answer to question 5")
+// 5. Multiply all values from 1 to 12 togeher and display answer //
+var product = 1;
+for (var i = 1; i < 13; i++) {
+    product *= i;
+}
+console.log(product);
+//---------------------------------------------------------------//
+
+console.log("-------------------------------------")
